7d25279a9f6d18ba73e49c0e804fa5571241d2c3
Ali Hussein <ah0979075074@gmail.com>
2025-06-15 01:46:23 +0300

    add a registration page

---
+++ ExpenseTracker/app_tracker/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('account/register/', views.register, name='register'),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
--- ExpenseTracker/app_tracker/views.py
+++ ExpenseTracker/app_tracker/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .models import Account, Expense
+from django.views.generic import TemplateView   #Provides a generic class-based view to render a static or context-driven template
+from django.views.generic.edit import FormView  #CBV for forms
+from django.views.generic  import ListView      #CBV for displaying list of objects
+from datetime import datetime
 
-# Create your views here.
+def home(request):
+    return render(request, "home/home.html")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form':form})
